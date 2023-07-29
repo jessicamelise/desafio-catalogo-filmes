5499039ca215aad5cece5c41ecb1d6bb187bcefa
jessicamelise <jessica.melise@hotmail.com>
2023-07-29 16:23:21 -0300

    feat: static home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { RouterProvider, createHashRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import Films from './pages/Films';
 import FilmDetail from './pages/FilmDetail';
+import { Box } from '@mui/material';
 
 const App = () => {
   const router = createHashRouter(
@@ -23,7 +24,17 @@ const App = () => {
   );
 
   return (
-    <RouterProvider router={router} />
+    <Box 
+      padding="30px 40px"
+      width="calc(100% - 80px)"
+      height="calc(100% - 60px)" 
+      sx={{
+        background: '#0B1014',
+      }}
+    >
+      <RouterProvider router={router} />
+
+    </Box>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,99 @@
-import { Box } from "@mui/material";
+import { Box, Button, TextField, ThemeProvider, createTheme, useMediaQuery } from "@mui/material";
+import logo from '../assets/images/logo.svg';
+import { useNavigate } from "react-router-dom";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#AA2321',
+    },
+    secondary: {
+      main: '#7B8C98',
+    },
+  },
+});
 
 const Home = () => {
+  const navigate = useNavigate();
+  const matches = useMediaQuery('(min-width:800px)');
+  const boxAttr = {
+    root: {
+      matches: {
+        alignItems: 'center',
+      },
+      notMatches: {
+        flexDirection: 'column',
+      },
+    },
+    img: {
+      matches: {
+        width: '60%'
+      },
+      notMatches: {
+        height: '50%',
+        display: 'flex',
+      },
+    },
+    form: {
+      matches: {
+        width: '40%',
+      },
+      notMatches: {
+        height: '50%',
+      },
+    },
+  };
+
+  const routeChange = () =>{ 
+    let path = `films`; 
+    navigate(path);
+  }
   
   return (
-    <Box>teste home</Box>    
+    <Box 
+      display="flex"
+      width="100%"
+      height="100%"
+      textAlign="center"
+      sx={matches ? boxAttr.root.matches : boxAttr.root.notMatches}
+    >
+      <Box sx={matches ? boxAttr.img.matches : boxAttr.img.notMatches}>
+        <Box component="img" src={logo} />
+      </Box>
+      <ThemeProvider theme={theme}>
+        <Box
+          component="form"
+          display="flex"
+          flexDirection="column"
+          gap="10px"
+          sx={{
+            '& .MuiInputBase-colorPrimary': { background: 'white' },
+            ...matches ? boxAttr.form.matches : boxAttr.form.notMatches,
+          }}
+          >
+          <TextField
+            variant="outlined"
+            size="small"
+            placeholder="email"
+            value=""
+          />
+          <TextField 
+            variant="outlined"
+            size="small"
+            placeholder="password"
+            value=""
+          />
+          <Button 
+            variant="contained" 
+            size="small"
+            color="primary"
+            onClick={routeChange}
+          >
+            Log in
+          </Button>
+        </Box>
+      </ThemeProvider>
+    </Box>    
   );
 }
   
